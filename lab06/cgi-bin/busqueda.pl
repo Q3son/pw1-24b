fa1dfd3f6cbecd5fac9a0e08627a06b2530a76f5
Giovanni Mejia <gmejiar@unsa.edu.pe>
2024-11-16 05:44:16 -0500

    Version primitiva para el html

--- lab06/cgi-bin/busqueda.pl
+++ lab06/cgi-bin/busqueda.pl
@@ -3,26 +3,50 @@
 use strict;
 use warnings;
 use utf8;
-use open qw(:std :utf8); # Configura entrada/salida estándar como UTF-8
-use Encode;             # Manejo explícito de codificaciones
+use CGI qw(:standard);
+use Encode;
 
-# Declaración de variables
-my $nombre_universidad = "Universidad Nacional Autónoma de México"; # Nombre de la universidad
-my $pais = "México"; # País
-my $ranking_min = 1; # Ranking mínimo
-my $ranking_max = 100; # Ranking máximo
+# Configuración UTF-8 para entrada/salida
+use open ':std', ':encoding(UTF-8)';
+binmode STDIN, ':encoding(UTF-8)';
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDERR, ':encoding(UTF-8)';
 
-# Configurar salida explícitamente a UTF-8
-binmode(STDOUT, ":encoding(utf8)");
+# Crear objeto CGI
+my $cgi = CGI->new;
 
-# Mostrar valores seleccionados
-print "Criterios de búsqueda:\n";
-print "Nombre de la universidad: $nombre_universidad\n";
-print "País: $pais\n";
-print "Ranking mínimo: $ranking_min\n";
-print "Ranking máximo: $ranking_max\n";
+# Enviar encabezado HTTP con UTF-8
+print $cgi->header(-type => 'text/html', -charset => 'UTF-8');
 
-# Placeholder para lógica adicional
-print "\n(Placeholder) Aquí realizarías la búsqueda con los criterios proporcionados.\n";
+# Leer parámetros CGI
+my $nombre_universidad         = decode_utf8($cgi->param('NOMBRE') || '');
+my $tipo_gestion               = decode_utf8($cgi->param('TIPO_GESTION') || '');
+my $estado_licenciamiento      = decode_utf8($cgi->param('ESTADO_LICENCIAMIENTO') || '');
+my $fecha_inicio_licenciamiento = decode_utf8($cgi->param('FECHA_INICIO') || '');
+my $fecha_fin_licenciamiento   = decode_utf8($cgi->param('FECHA_FIN') || '');
+my $periodo_licenciamiento     = decode_utf8($cgi->param('PERIODO_LICENCIAMIENTO') || '');
+my $departamento               = decode_utf8($cgi->param('DEPARTAMENTO') || '');
+my $provincia                  = decode_utf8($cgi->param('PROVINCIA') || '');
+my $distrito                   = decode_utf8($cgi->param('DISTRITO') || '');
 
-exit;
+# Imprimir datos en HTML
+print <<HTML;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Información de la Universidad</title>
+</head>
+<body>
+    <p>universidad: $nombre_universidad</p>
+    <p>tipo gestion: $tipo_gestion</p>
+    <p>estado de licenciamiento: $estado_licenciamiento</p>
+    <p>fecha inicio licenciamiento: $fecha_inicio_licenciamiento</p>
+    <p>fecha fin licenciamiento: $fecha_fin_licenciamiento</p>
+    <p>periodo licenciamiento: $periodo_licenciamiento</p>
+    <p>departamento: $departamento</p>
+    <p>provincia: $provincia</p>
+    <p>distrito: $distrito</p>
+</body>
+</html>
+HTML
